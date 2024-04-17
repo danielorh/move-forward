1f9b8ef067678259974762c7f75e300b317317ec
danielorh <156919200+danielorh@users.noreply.github.com>
2024-04-17 22:04:28 +0200

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -3,23 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-import requests
 
 application = Flask(__name__)
 
-# secret key
-application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
+### Code GitHub
+application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+DBVAR = 'postgresql://username:password@endpoint:5432//ebdb'
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
-# database-configurations
-application.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
-application.config["SQLALCHEMY_BINDS"] = {"transport" : "sqlite:///transport.db"}
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # removes error-message
 db = SQLAlchemy(application)
-
-# encrypting passwords in database
 bcrypt = Bcrypt(application)
-
-# flask login-manager
 login_manager= LoginManager(application)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
@@ -28,10 +23,8 @@ from capp.home.routes import home
 from capp.methodology.routes import methodology
 from capp.carbon_calc.routes import carbon_calc
 from capp.users.routes import users
-from capp.carbon_interface.routes import carbon_interface
 
 application.register_blueprint(home)
 application.register_blueprint(methodology)
 application.register_blueprint(carbon_calc)
 application.register_blueprint(users)
-application.register_blueprint(carbon_interface)
